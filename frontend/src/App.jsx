0d4899fe93dc79b7c0cc286f6ec7e29ae428350e
Shikai Liu <lsk569937453@gmail.com>
2024-01-06 00:40:21 +0800

    Update the git-staistic.

---
+++ frontend/src/App.jsx
@@ -0,0 +1,66 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import {Button} from "@nextui-org/react";
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+      <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    <Button variant="bordered" radius="md">
+        Button
+      </Button>
+      <Button isDisabled color="primary" radius="md">
+        Disabled
+      </Button>
+      <Button color="primary">Solid</Button>
+      <Button color="primary" variant="ghost">Ghost</Button>
+    <Button
+      disableRipple
+      className="relative overflow-visible rounded-full hover:-translate-y-1 px-12 shadow-xl bg-background/30 after:content-[''] after:absolute after:rounded-full after:inset-0 after:bg-background/40 after:z-[-1] after:transition after:!duration-500 hover:after:scale-150 hover:after:opacity-0"
+      size="lg"
+    >
+      Press me
+    </Button>
+    <Button color="secondary">
+        Secondary
+      </Button>  
+      <Button color="success">
+        Success
+      </Button>  
+      <Button color="warning">
+        Warning
+      </Button>  
+      <Button color="danger" className='focus:outline-none'>
+        Danger
+      </Button>  
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import {NextUIProvider} from '@nextui-org/react'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <NextUIProvider>
+    <App />
+    </NextUIProvider>
+  </React.StrictMode>,
+)
