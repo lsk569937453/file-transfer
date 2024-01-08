200466b052e42b740293494ea3aaa94f39b09ebd
shikai liu <lsk569937453@gmail.com>
2024-01-08 16:29:23 +0800

    Fix bugs.

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -21,7 +21,6 @@ module.exports = konstaConfig({
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
 
   ],
   theme: {
